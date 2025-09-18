b2df9c74291b698f085038d2ea0d8d538cf04ed1
Adam Smedberg <asmedberg@gmail.com>
2025-09-17 18:26:38 -0700

    install gsap and add split text to intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
-import Section from "@/components/Section";
+import Section from "@/components/layout/Section";
+import Intro from "@/components/sections/Intro";
 
 export default function Home() {
   return (
     <main>
       <Section>
-        <div className="grow flex flex-col justify-center">
-          <h1 className="font-bold text-3xl">Hi, I&rsquo;m Adam</h1>
-          <p className="mt-2 text-pretty">
-            I&rsquo;m a web developer specializing in building modern web applications.
-          </p>
-          <p className="mt-2">Here is some of my work:</p>
-        </div>
+        <Intro />
       </Section>
     </main>
   );
