f0bdcb3b785a34aaae5a7c0ab7836246c2fde276
Adam Smedberg <asmedberg@gmail.com>
2025-09-18 11:10:42 -0700

    scope intro gsap to container

--- src/components/sections/Intro.tsx
+++ src/components/sections/Intro.tsx
@@ -1,40 +1,46 @@
 "use client";
-// import { useRef } from "react";
+import { useRef } from "react";
 import { gsap } from "gsap";
 import { SplitText } from "gsap/dist/SplitText";
 import { useGSAP } from "@gsap/react";
 
 export default function Intro() {
-  // const container = useRef<HTMLDivElement>(null);
+  const container = useRef<HTMLDivElement>(null);
 
-  useGSAP(() => {
-    gsap.registerPlugin(SplitText);
-    SplitText.create(".split", {
-      type: "words",
-      autoSplit: true,
-      lineClass: "mt-0",
-      onSplit: self => {
-        return gsap.from(self.words, {
-          y: 20,
-          opacity: 0,
-          stagger: 0.05,
-          onComplete: () => self.revert()
-        });
-      }
-    });
-  });
+  useGSAP(
+    () => {
+      gsap.registerPlugin(SplitText);
+
+      SplitText.create(".split", {
+        type: "words",
+        autoSplit: true,
+        lineClass: "mt-0",
+        onSplit: self => {
+          return gsap.from(self.words, {
+            y: 20,
+            opacity: 0,
+            stagger: 0.05,
+            onComplete: () => self.revert()
+          });
+        }
+      });
+    },
+    { scope: container }
+  );
 
   return (
-    <div className="grow flex flex-col justify-center split">
-      <h1 className="mb-2 font-bold text-3xl sm:text-5xl lg:text-7xl">
-        Hi! 👋
-        <br />
-        I&rsquo;m Adam
-      </h1>
-      <p className="text-lg leading-tight text-pretty">
-        I&rsquo;m a web developer specializing in building modern web applications.
-      </p>
-      <p className="text-lg mt-2">Scroll to see some of my work.</p>
+    <div ref={container} className="grow flex flex-col justify-center">
+      <div className="split">
+        <h1 className="mb-2 font-bold text-3xl sm:text-5xl lg:text-7xl">
+          Hi! 👋
+          <br />
+          I&rsquo;m Adam
+        </h1>
+        <p className="text-lg leading-tight text-pretty">
+          I&rsquo;m a web developer specializing in building modern web applications.
+        </p>
+        <p className="text-lg mt-2">Scroll to see some of my work.</p>
+      </div>
     </div>
   );
 }
