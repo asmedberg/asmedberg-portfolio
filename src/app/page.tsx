1bc31a6a0ff633a69b44cf49e697d3817e0ee2ab
Adam Smedberg <asmedberg@gmail.com>
2025-10-06 16:14:24 -0700

    add projects using json file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,15 @@
-import Link from "next/link";
-import Image from "next/image";
-
-import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
-
-import Section from "@/components/layout/Section";
 import Intro from "@/components/sections/Intro";
 import Project from "@/components/Project";
 
-import fgcoScreenshot from "@/assets/fgco-screenshot.png";
+import projects from "@/projects.json";
 
 export default function Home() {
   return (
     <main>
       <Intro />
-      <Section>
-        <Project>
-          <Project.Image>
-            <Image src={fgcoScreenshot} alt="Screenshot from forgoodandco.com" />
-          </Project.Image>
-          <Project.Content>
-            <Project.Title>For Good &amp; Company - Website</Project.Title>
-            <Project.Description>
-              This is an agency site to showcase their work and services. I worked closely with the creative team to on
-              the design and development of the site. Creative handed off Figma designs for the look and feel and
-              components for the site. I chose to use Next.js
-            </Project.Description>
-            <Project.Tools list={["NextJS", "React", "Sanity.io", "JSX", "CSS"]} />
-            <Project.Links>
-              <Link
-                href="https://forgoodandco.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex flex-row flex-nowrap gap-2 items-center px-4 py-2 rounded-full border-2 border-blue-400 font-bold text-blue-400 hover:bg-blue-400 hover:text-background"
-              >
-                View Site <ArrowTopRightOnSquareIcon className="w-4 h-4" />
-              </Link>
-            </Project.Links>
-          </Project.Content>
-        </Project>
-      </Section>
+      {projects.map(project => (
+        <Project key={project.title} {...project} />
+      ))}
     </main>
   );
 }
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,42 +1,81 @@
-const ProjectImage = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex-2 rounded-lg border-2 border-slate-400 overflow-hidden">{children}</div>;
-};
+import Image from "next/image";
+import Link from "next/link";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
+import Section from "./layout/Section";
 
-const ProjectContent = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex-1 flex flex-col gap-4">{children}</div>;
-};
-
-const ProjectTitle = ({ children }: { children: React.ReactNode }) => {
-  return <h1 className="text-3xl font-bold text-pretty">{children}</h1>;
-};
-
-const ProjectDescription = ({ children }: { children: React.ReactNode }) => {
-  return <p className="text-balance">{children}</p>;
-};
-
-const ProjectTools = ({ list }: { list: string[] }) => {
-  return (
-    <div className="flex flex-row flex-wrap gap-2">
-      {list.map(item => (
-        <span key={item} className="px-3 py-1 text-sm font-bold border border-slate-400 text-slate-400 rounded-full">
-          {item}
-        </span>
-      ))}
-    </div>
-  );
-};
-
-const ProjectLinks = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex flex-row flex-wrap gap-2">{children}</div>;
-};
-
-export default function Project({ children }: { children: React.ReactNode }) {
-  return <div className="grow flex flex-col gap-8 md:flex-row md:items-center">{children}</div>;
+interface ProjectProps {
+  image?: {
+    source: string;
+    alt: string;
+    width: number;
+    height: number;
+  };
+  video?: {
+    source: string;
+    title: string;
+  };
+  title: string;
+  description: string[];
+  tools?: string[];
+  links?: {
+    project?: string;
+    code?: string;
+  };
 }
 
-Project.Image = ProjectImage;
-Project.Content = ProjectContent;
-Project.Title = ProjectTitle;
-Project.Description = ProjectDescription;
-Project.Tools = ProjectTools;
-Project.Links = ProjectLinks;
+export default function Project(props: ProjectProps) {
+  const { image, title, description, tools, links } = props;
+  return (
+    <Section>
+      <div className="grow flex flex-col gap-8 md:flex-row md:items-center">
+        <div className="flex-2 rounded-lg border-2 border-slate-400 overflow-hidden">
+          {image && <Image src={image.source} alt={image.alt} width={image.width} height={image.height} />}
+        </div>
+        <div className="flex-1 flex flex-col gap-4">
+          <h1 className="text-3xl font-bold text-pretty">{title}</h1>
+          <div className="space-y-2">
+            {description.map((desc, i) => (
+              <p key={i} className="text-balance">
+                {desc}
+              </p>
+            ))}
+          </div>
+          <div className="flex flex-row flex-wrap gap-2">
+            {tools?.map(item => (
+              <span
+                key={item}
+                className="px-3 py-1 text-sm font-bold border border-slate-400 text-slate-400 rounded-full"
+              >
+                {item}
+              </span>
+            ))}
+          </div>
+          {(links?.project || links?.code) && (
+            <div className="flex flex-row flex-wrap gap-2">
+              {links.project && (
+                <Link
+                  href={links.project}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex flex-row flex-nowrap gap-2 items-center px-4 py-2 rounded-full border-2 border-blue-400 font-bold text-blue-400 hover:bg-blue-400 hover:text-background"
+                >
+                  View Site <ArrowTopRightOnSquareIcon className="w-4 h-4" />
+                </Link>
+              )}
+              {links.code && (
+                <Link
+                  href={links.code}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex flex-row flex-nowrap gap-2 items-center px-4 py-2 rounded-full border-2 border-blue-400 font-bold text-blue-400 hover:bg-blue-400 hover:text-background"
+                >
+                  View Code <ArrowTopRightOnSquareIcon className="w-4 h-4" />
+                </Link>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </Section>
+  );
+}
