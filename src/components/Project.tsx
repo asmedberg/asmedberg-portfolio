dba73f763142c99d301579774ef65737e27b3350
Adam Smedberg <asmedberg@gmail.com>
2025-10-13 14:23:39 -0700

    update project layout

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -29,31 +29,21 @@ export default function Project(props: ProjectProps) {
   const { image, video, title, description, tools, links } = props;
   return (
     <Section>
-      <div className="grow flex flex-col gap-8">
+      <div className="grow flex flex-col gap-4">
         <h1 className="text-3xl font-bold text-pretty">{title}</h1>
-        <div className="flex flex-col gap-3">
-          <div className="flex-2 rounded-lg border-2 border-slate-400 overflow-hidden">
-            {image && (
-              <Image
-                urlEndpoint={imagekit_endpoint}
-                src={image.source}
-                alt={image.alt}
-                width={image.width}
-                height={image.height}
-              />
-            )}
-            {video && <Video urlEndpoint={imagekit_endpoint} src={video.source} title={video.title} controls />}
-          </div>
+        <div className="rounded-lg border-2 border-slate-400 overflow-hidden">
+          {image && (
+            <Image
+              urlEndpoint={imagekit_endpoint}
+              src={image.source}
+              alt={image.alt}
+              width={image.width}
+              height={image.height}
+            />
+          )}
+          {video && <Video urlEndpoint={imagekit_endpoint} src={video.source} title={video.title} controls />}
         </div>
         <div className="flex flex-col gap-4">
-          <div className="space-y-4 md:columns-2">
-            {description.map((desc, i) => (
-              <p key={i} className="text-pretty">
-                {desc}
-              </p>
-            ))}
-          </div>
-
           <div className="flex flex-row flex-wrap gap-2">
             {tools?.map(item => (
               <span
@@ -62,6 +52,13 @@ export default function Project(props: ProjectProps) {
               >
                 {item}
               </span>
+            ))}
+          </div>
+          <div className="space-y-4 md:columns-2">
+            {description.map((desc, i) => (
+              <p key={i} className="text-pretty">
+                {desc}
+              </p>
             ))}
           </div>
 
