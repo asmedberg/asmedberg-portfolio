4d6ce26a63a937a90338a90468fab6bf0022d67a
Adam Smedberg <asmedberg@gmail.com>
2025-10-14 18:12:40 -0700

    adds mouse position to background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "next-themes";
 import ThemeToggler from "@/components/ThemeToggler";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavDetails from "@/components/NavDetails";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`bg-light text-dark dark:bg-dark dark:text-light ${geistSans.variable} ${geistMono.variable} font-medium md:text-lg antialiased`}
       >
         <ThemeProvider attribute="class">
+          <NavDetails />
           <div className="p-6 bg-[url('/images/contour-map-light.svg')] dark:bg-[url('/images/contour-map.svg')] bg-top bg-cover">
             <ThemeToggler />
             {children}
