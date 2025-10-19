86abe6bf3b39823c3b409eca121c590508e32b1c
Adam Smedberg <asmedberg@gmail.com>
2025-10-19 16:15:23 -0700

    add github link

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,20 @@
+import Link from "next/link";
 import ThemeToggler from "@/components/ThemeToggler";
 
 export default function Header() {
   return (
-    <header className="absolute top-0 w-full p-4 flex flex-row flex-nowrap items-center justify-end">
+    <header className="absolute top-0 w-full p-4 flex flex-row flex-nowrap items-start justify-end gap-x-2">
+      <Link
+        href="https://github.com/asmedberg"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={[
+          "text-sm px-2 py-1 rounded-full border-2 border-dark text-dark hover:bg-dark hover:text-light",
+          "dark:text-light dark:border-light dark:hover:bg-light dark:hover:text-dark"
+        ].join(" ")}
+      >
+        Github
+      </Link>
       <ThemeToggler />
     </header>
   );
