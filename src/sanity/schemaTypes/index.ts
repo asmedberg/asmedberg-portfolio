b78295f2610f87ed0c1b6409b662a532e53e97ce
Adam Smedberg <asmedberg@gmail.com>
2025-10-20 19:40:15 -0700

    add settings singleton schema

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -4,9 +4,12 @@ import blockContent from "./objects/blockContent";
 import projectImage from "./objects/image";
 import projectVideo from "./objects/video";
 import link from "./objects/link";
+import settings from "./singletons/settings";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
+    // singletons
+    settings,
     // documents
     projects,
     // objects
---
+++ src/sanity/schemaTypes/singletons/settings.ts
@@ -0,0 +1,69 @@
+import { CogIcon } from "@sanity/icons";
+import { defineType, defineField, defineArrayMember } from "sanity";
+
+export default defineType({
+  name: "settings",
+  title: "Settings",
+  icon: CogIcon,
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string"
+    }),
+    defineField({
+      name: "intro",
+      title: "Introduction",
+      type: "blockContent"
+    }),
+    defineField({
+      name: "projectOrder",
+      title: "Project Order",
+      type: "array",
+      of: [
+        defineArrayMember({
+          type: "reference",
+          to: [{ type: "projects" }]
+        })
+      ]
+    }),
+    defineField({
+      name: "seo",
+      title: "SEO",
+      type: "object",
+      fields: [
+        defineField({
+          name: "title",
+          title: "Site title",
+          type: "string"
+        }),
+        defineField({
+          name: "description",
+          title: "Site description",
+          type: "text"
+        }),
+        defineField({
+          name: "keywords",
+          title: "Site keywords",
+          type: "array",
+          of: [{ type: "string" }],
+          options: {
+            layout: "tags"
+          }
+        }),
+        defineField({
+          name: "author",
+          title: "Site author",
+          type: "string"
+        }),
+        defineField({
+          name: "image",
+          title: "Site image",
+          type: "image",
+          description: "This image will be used for sharing previews on social media. Dimensions: 1200x630px"
+        })
+      ]
+    })
+  ]
+});
