c1dd673214fcf6c59ddc62166327ade00cf44773
Adam Smedberg <asmedberg@gmail.com>
2025-10-24 09:32:55 -0700

    add fetch for projects

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,30 +1,14 @@
-import { client } from "@/sanity/lib/client";
-import { PROJECTS_QUERY } from "@/sanity/lib/queries";
-import { PROJECTS_QUERYResult } from "@/sanity/types/sanity.types";
+import { fetchProjects } from "@/sanity/lib/fetch";
 import Intro from "@/components/sections/Intro";
 import Project, { type ProjectProps } from "@/components/Project";
 
-const getProjects = async () => {
-  try {
-    const projects = await client.fetch<PROJECTS_QUERYResult>(PROJECTS_QUERY);
-    if (!projects) {
-      throw new Error("Projects not returned");
-    }
-    return projects;
-  } catch (error) {
-    return console.error(error);
-  }
-};
-
 export default async function Home() {
-  const projects = await getProjects();
+  const projects = await fetchProjects();
 
   return (
     <main>
       <Intro />
-      {(projects as ProjectProps[])?.map(project => (
-        <Project key={project._id} {...project} />
-      ))}
+      {projects && (projects as ProjectProps[])?.map(project => <Project key={project._id} {...project} />)}
     </main>
   );
 }
--- src/sanity/lib/fetch.ts
+++ src/sanity/lib/fetch.ts
@@ -1,6 +1,6 @@
 import { sanityFetch } from "@/sanity/lib/live";
-import { INTRO_QUERY, RESUME_QUERY } from "@/sanity/lib/queries";
-import { INTRO_QUERYResult, RESUME_QUERYResult } from "@/sanity/types/sanity.types";
+import { INTRO_QUERY, PROJECTS_QUERY, RESUME_QUERY } from "@/sanity/lib/queries";
+import { INTRO_QUERYResult, PROJECTS_QUERYResult, RESUME_QUERYResult } from "@/sanity/types/sanity.types";
 
 export const fetchResume = async (): Promise<RESUME_QUERYResult> => {
   const { data } = await sanityFetch({ query: RESUME_QUERY });
@@ -10,6 +10,12 @@ export const fetchResume = async (): Promise<RESUME_QUERYResult> => {
 
 export const fetchIntro = async (): Promise<INTRO_QUERYResult> => {
   const { data } = await sanityFetch({ query: INTRO_QUERY });
+
+  return data;
+};
+
+export const fetchProjects = async (): Promise<PROJECTS_QUERYResult> => {
+  const { data } = await sanityFetch({ query: PROJECTS_QUERY });
 
   return data;
 };
