88d4187998be56b3cad96181880d86888298d96a
Adam Smedberg <asmedberg@gmail.com>
2025-10-23 21:38:45 -0700

    update sanity fetching for resume and intro

--- src/components/sections/Intro.tsx
+++ src/components/sections/Intro.tsx
@@ -1,8 +1,6 @@
 import { PortableText, PortableTextReactComponents } from "next-sanity";
-import { client } from "@/sanity/lib/client";
-import { INTRO_QUERY } from "@/sanity/lib/queries";
 import Section from "@/components/layout/Section";
-import { type INTRO_QUERYResult } from "@/sanity/types/sanity.types";
+import { fetchIntro } from "@/sanity/lib/fetch";
 
 const introComponents: Partial<PortableTextReactComponents> = {
   block: {
@@ -12,27 +10,13 @@ const introComponents: Partial<PortableTextReactComponents> = {
   }
 };
 
-const getIntro = async () => {
-  try {
-    const intro = await client.fetch<INTRO_QUERYResult>(INTRO_QUERY);
-    if (!intro?.content) {
-      throw new Error("Content not returned");
-    }
-    return intro?.content;
-  } catch (error) {
-    console.error(`Something went wrong; ${error}`);
-  }
-};
-
 export default async function Intro() {
-  const content = await getIntro();
-
-  if (!content) return null;
+  const intro = await fetchIntro();
 
   return (
     <Section>
       <div className="grow flex flex-col justify-center">
-        <PortableText value={content} components={introComponents} />
+        {intro?.content && <PortableText value={intro.content} components={introComponents} />}
       </div>
     </Section>
   );
---
+++ src/sanity/lib/fetch.ts
@@ -0,0 +1,15 @@
+import { sanityFetch } from "@/sanity/lib/live";
+import { INTRO_QUERY, RESUME_QUERY } from "@/sanity/lib/queries";
+import { INTRO_QUERYResult, RESUME_QUERYResult } from "@/sanity/types/sanity.types";
+
+export const fetchResume = async (): Promise<RESUME_QUERYResult> => {
+  const { data } = await sanityFetch({ query: RESUME_QUERY });
+
+  return data;
+};
+
+export const fetchIntro = async (): Promise<INTRO_QUERYResult> => {
+  const { data } = await sanityFetch({ query: INTRO_QUERY });
+
+  return data;
+};
